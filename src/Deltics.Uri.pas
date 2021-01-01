1bd304402d7aabb582bf861fc7b6636bb8bdfc64
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-01-02 10:47:36 +1300

    First packaged release (#1)
    * test: Added tests
    * fix: Record compatibility with older Delphi versions
    * deps: Updated to Deltics.Strings 1.1.1

---
+++ src/Deltics.Uri.pas
@@ -0,0 +1,324 @@
+
+  unit Deltics.Uri;
+
+interface
+
+  uses
+    Deltics.InterfacedObjects,
+    Deltics.Strings;
+
+  type
+    IUri = interface
+    ['{D1889DA2-096A-4019-A502-D0EFDA1078AB}']
+      function get_AsFilePath: String;
+      function get_AsString: String;
+      function get_Authority: String;
+      function get_Scheme: String;
+      function get_UserInfo: String;
+      function get_Host: String;
+      function get_Port: Integer;
+      function get_Path: String;
+      function get_Query: String;
+      function get_Fragment: String;
+      procedure set_AsString(const aValue: String);
+      procedure set_Scheme(const aValue: String);
+      procedure set_UserInfo(const aValue: String);
+      procedure set_Host(const aValue: String);
+      procedure set_Port(const aValue: Integer);
+      procedure set_Path(const aValue: String);
+      procedure set_Query(const aValue: String);
+      procedure set_Fragment(const aValue: String);
+
+      property Scheme: String read get_Scheme write set_Scheme;
+      property UserInfo: String read get_UserInfo write set_UserInfo;
+      property Host: String read get_Host write set_Host;
+      property Port: Integer read get_Port write set_Port;
+      property Path: String read get_Path write set_Path;
+      property Query: String read get_Query write set_Query;
+      property Fragment: String read get_Fragment write set_Fragment;
+      property Authority: String read get_Authority;
+      property AsFilePath: String read get_AsFilePath;
+      property AsString: String read get_AsString write set_AsString;
+    end;
+
+
+    TUri = class(TComInterfacedObject, IUri)
+    // IUri
+    protected
+      function get_AsFilePath: String;
+      function get_AsString: String;
+      function get_Authority: String;
+      function get_Scheme: String;
+      function get_UserInfo: String;
+      function get_Host: String;
+      function get_Port: Integer;
+      function get_Path: String;
+      function get_Query: String;
+      function get_Fragment: String;
+      procedure set_AsString(const aValue: String);
+      procedure set_Scheme(const aValue: String);
+      procedure set_UserInfo(const aValue: String);
+      procedure set_Host(const aValue: String);
+      procedure set_Port(const aValue: Integer);
+      procedure set_Path(const aValue: String);
+      procedure set_Query(const aValue: String);
+      procedure set_Fragment(const aValue: String);
+
+    private
+      fScheme: String;
+      fUserInfo: String;
+      fHost: String;
+      fPort: Integer;
+      fPath: String;
+      fQuery: String;
+      fFragment: String;
+    public
+      constructor Create; overload;
+      constructor Create(const aUri: String); overload;
+      property Scheme: String read fScheme write fScheme;
+      property UserInfo: String read get_UserInfo write set_UserInfo;
+      property Host: String read fHost write fHost;
+      property Port: Integer read fPort write fPort;
+      property Path: String read fPath write fPath;
+      property Query: String read fQuery write fQuery;
+      property Fragment: String read fFragment write fFragment;
+      property Authority: String read get_Authority;
+      property AsFilePath: String read get_AsFilePath;
+      property AsString: String read get_AsString write set_AsString;
+    end;
+
+
+implementation
+
+  uses
+    SysUtils,
+    Deltics.Exceptions,
+    Deltics.Strings.Templates;
+
+
+{ TUri }
+
+  constructor TUri.Create(const aUri: String);
+  begin
+    inherited Create;
+
+    AsString := aUri;
+  end;
+
+
+  constructor TUri.Create;
+  begin
+    inherited Create;
+
+    fPort := -1;
+  end;
+
+
+  function TUri.get_AsFilePath: String;
+  begin
+    if fScheme <> 'file' then
+      raise EInvalidOperation.CreateFmt('''%s'' is not a filepath Uri', [AsString]);
+
+    if fPath = '' then
+      raise EInvalidOperation.CreateFmt('''%s'' is not a valid filepath Uri', [AsString]);
+
+    if NOT STR.Contains(fPath, ':') then
+      result := '//' + fHost + '/' + fPath
+    else
+      result := fPath;
+
+    result := STR.ReplaceAll(result, '/', '\');
+  end;
+
+
+  function TUri.get_AsString: String;
+  var
+    auth: String;
+  begin
+    auth    := Authority;
+    result  := fScheme + '://';
+
+    if auth <> '' then
+      result := result + auth;
+
+    if fPath <> '' then
+      result := result + '/' + fPath;
+
+    if fQuery <> '' then
+      result := result + '?' + fQuery;
+
+    if fFragment <> '' then
+      result := result + '#' + fFragment;
+  end;
+
+
+  function TUri.get_Authority: String;
+  begin
+    result := '';
+
+    if fUserInfo <> '' then
+      result := result + fUserInfo + '@';
+
+    if fHost <> '' then
+      result := result + fHost;
+
+    if fPort <> -1 then
+      result := result + ':' + IntToStr(fPort);
+  end;
+
+
+  function TUri.get_Fragment: String;
+  begin
+    result := fFragment;
+  end;
+
+  function TUri.get_Host: String;
+  begin
+    result := fHost;
+  end;
+
+  function TUri.get_Path: String;
+  begin
+    result := fPath;
+  end;
+
+  function TUri.get_Port: Integer;
+  begin
+    result := fPort;
+  end;
+
+  function TUri.get_Query: String;
+  begin
+    result := fQuery;
+  end;
+
+  function TUri.get_Scheme: String;
+  begin
+    result := fScheme;
+  end;
+
+  function TUri.get_UserInfo: String;
+  begin
+    result := fUserInfo;
+  end;
+
+
+
+  procedure TUri.set_AsString(const aValue: String);
+  var
+    vars: TStringList;
+  begin
+    fScheme   := '';
+    fUserInfo := '';
+    fHost     := '';
+    fPort     := -1;
+    fPath     := '';
+    fQuery    := '';
+    fFragment := '';
+
+    vars := TStringList.Create;
+    try
+      if TStringTemplate.Match([
+                                 '[scheme]://[userinfo]@[host]:[port:int]/[path]?[query]#[fragment]',
+                                 '[scheme]://[userinfo]@[host]:[port:int]/[path]?[query]',
+                                 '[scheme]://[userinfo]@[host]:[port:int]/[path]#[fragment]',
+                                 '[scheme]://[userinfo]@[host]:[port:int]/[path]',
+                                 '[scheme]://[userinfo]@[host]/[path]?[query]#[fragment]',
+                                 '[scheme]://[userinfo]@[host]/[path]?[query]',
+                                 '[scheme]://[userinfo]@[host]/[path]?#[fragment]',
+                                 '[scheme]://[userinfo]@[host]/[path]',
+                                 '[scheme]://[host]:[port:int]/[path]?[query]#[fragment]',
+                                 '[scheme]://[host]:[port:int]/[path]?[query]',
+                                 '[scheme]://[host]:[port:int]/[path]?#[fragment]',
+                                 '[scheme]://[host]:[port:int]/[path]',
+                                 '[scheme]:///[path]?[query]#[fragment]',
+                                 '[scheme]:///[path]?[query]',
+                                 '[scheme]:///[path]?#[fragment]',
+                                 '[scheme]:///[path]',
+                                 '[scheme]://[host]/[path]?[query]#[fragment]',
+                                 '[scheme]://[host]/[path]?[query]',
+                                 '[scheme]://[host]/[path]?#[fragment]',
+                                 '[scheme]://[host]/[path]',
+                                 '[scheme]://[host]/',
+                                 '[scheme]://[host]',
+                                 '\\[host]\[path]',
+                                 '[drive]:\[path]',
+                                 '\\\[path]'
+                               ], aValue, vars) then
+      begin
+        if vars.ContainsName('scheme') then
+        begin
+          fScheme := vars.Values['scheme'];
+
+          if vars.ContainsName('userinfo')  then UserInfo   := vars.Values['userinfo'];
+          if vars.ContainsName('host')      then fHost      := vars.Values['host'];
+          if vars.ContainsName('port')      then fPort      := STR.AsInteger(vars.Values['port']);
+          if vars.ContainsName('path')      then fPath      := vars.Values['path'];
+          if vars.ContainsName('query')     then fQuery     := vars.Values['query'];
+          if vars.ContainsName('fragment')  then fFragment  := vars.Values['fragment'];
+        end
+        else if vars.ContainsName('drive') then
+        begin
+          fScheme := 'file';
+          fPath   := vars.Values['drive'] + ':/';
+          if vars.ContainsName('path') then
+            fPath := fPath + STR.ReplaceAll(vars.Values['path'], '\', '/');
+        end
+        else if vars.ContainsName('path') then
+        begin
+          fScheme := 'file';
+
+          if vars.ContainsName('host') then
+            fHost := vars.Values['host'];
+
+          fPath := STR.ReplaceAll(vars.Values['path'], '\', '/');
+        end;
+      end;
+
+    finally
+      vars.Free;
+    end;
+  end;
+
+
+  procedure TUri.set_Fragment(const aValue: String);
+  begin
+    fFragment := aValue;
+  end;
+
+  procedure TUri.set_Host(const aValue: String);
+  begin
+    fHost := aValue;
+  end;
+
+  procedure TUri.set_Path(const aValue: String);
+  begin
+    fPath := aValue;
+  end;
+
+  procedure TUri.set_Port(const aValue: Integer);
+  begin
+    fPort := aValue;
+  end;
+
+  procedure TUri.set_Query(const aValue: String);
+  begin
+    fQuery := aValue;
+  end;
+
+  procedure TUri.set_Scheme(const aValue: String);
+  begin
+    fScheme := aValue;
+  end;
+
+  procedure TUri.set_UserInfo(const aValue: String);
+  begin
+    fUserInfo := aValue;
+
+    if STR.EndsWith(fUserInfo, ':') then
+      fUserInfo := STR.RTrim(fUserInfo, 1);
+  end;
+
+
+
+end.
---
+++ tests/Uri.Tests.pas
@@ -0,0 +1,214 @@
+
+  unit Uri.Tests;
+
+interface
+
+  uses
+    Deltics.Smoketest;
+
+
+  type
+    UriValues = record
+      Scheme: String;
+      UserInfo: String;
+      Host: String;
+      Port: Integer;
+      Path: String;
+      Query: String;
+      Fragment: String;
+      Authority: String;
+    end;
+
+    _UriValues = class
+      class function Init(const aScheme: String; const aUserInfo: String; const aHost: String; const aPort: Integer; const aPath: String; const aQuery: String; const aFragment: String; const aAuthority: String): UriValues;
+    end;
+
+
+    TUriTests = class(TTest)
+    private
+      procedure TestViaObject(const aUriString: String; const aComponents: UriValues);
+      procedure TestViaInterface(const aUriString: String; const aComponents: UriValues);
+    published
+      procedure UriValuesAreCorrectlyIdentifiedInStringsUsingObject;
+      procedure UriValuesAreCorrectlyIdentifiedInStringsUsingInterface;
+      procedure EmptyPasswordIsStrippedFromUserInfo;
+      procedure NonEmptyPasswordIsPreservedInUserInfo;
+      procedure UncPathIsParsedAsFileScheme;
+      procedure DriveLetterPathIsParsedAsFileScheme;
+      procedure AsPathReturnsFilepathForValidUris;
+      procedure AsPathRaisesInvalidOperationForInvalidUris;
+    end;
+
+
+
+implementation
+
+  uses
+    Deltics.Uri,
+    Deltics.Exceptions;
+
+
+
+  class function _UriValues.Init(const aScheme: String;
+                                 const aUserInfo: String;
+                                 const aHost: String;
+                                 const aPort: Integer;
+                                 const aPath: String;
+                                 const aQuery: String;
+                                 const aFragment: String;
+                                 const aAuthority: String): UriValues;
+  begin
+    with result do
+    begin
+      Scheme    := aScheme;
+      UserInfo  := aUserInfo;
+      Host      := aHost;
+      Port      := aPort;
+      Path      := aPath;
+      Query     := aQuery;
+      Fragment  := aFragment;
+      Authority := aAuthority;
+    end;
+  end;
+
+
+  procedure TUriTests.TestViaObject(const aUriString: String; const aComponents: UriValues);
+  var
+    uri: TUri;
+  begin
+    uri := TUri.Create(aUriString);
+    try
+      Test('Scheme').Assert(uri.Scheme).Equals(aComponents.Scheme);
+      Test('UserInfo').Assert(uri.UserInfo).Equals(aComponents.UserInfo);
+      Test('Host').Assert(uri.Host).Equals(aComponents.Host);
+      Test('Port').Assert(uri.Port).Equals(aComponents.Port);
+      Test('Path').Assert(uri.Path).Equals(aComponents.Path);
+      Test('Query').Assert(uri.Query).Equals(aComponents.Query);
+      Test('Fragment').Assert(uri.Fragment).Equals(aComponents.Fragment);
+      Test('Authority').Assert(uri.Authority).Equals(aComponents.Authority);
+
+    finally
+      uri.Free;
+    end;
+  end;
+
+  procedure TUriTests.TestViaInterface(const aUriString: String; const aComponents: UriValues);
+  var
+    uri: IUri;
+  begin
+    uri := TUri.Create(aUriString);
+
+    Test('Scheme').Assert(uri.Scheme).Equals(aComponents.Scheme);
+    Test('UserInfo').Assert(uri.UserInfo).Equals(aComponents.UserInfo);
+    Test('Host').Assert(uri.Host).Equals(aComponents.Host);
+    Test('Port').Assert(uri.Port).Equals(aComponents.Port);
+    Test('Path').Assert(uri.Path).Equals(aComponents.Path);
+    Test('Query').Assert(uri.Query).Equals(aComponents.Query);
+    Test('Fragment').Assert(uri.Fragment).Equals(aComponents.Fragment);
+    Test('Authority').Assert(uri.Authority).Equals(aComponents.Authority);
+  end;
+
+
+  procedure TUriTests.UriValuesAreCorrectlyIdentifiedInStringsUsingObject;
+  begin
+    TestViaObject('file:///c:/folder/subfolder', _UriValues.Init('file', '', '', -1, 'c:/folder/subfolder', '', '', ''));
+    TestViaObject('c:\duget\whatever',           _UriValues.Init('file', '', '', -1, 'c:/duget/whatever',   '', '', ''));
+
+    TestViaObject('http://api.duget.org/v1/index.json',                                _UriValues.Init('http', '',      'api.duget.org', -1, 'v1/index.json',               '',       '',     'api.duget.org'));
+    TestViaObject('http://user@hostname:443/api.duget.org/v1/index.json?latest#frag',  _UriValues.Init('http', 'user',  'hostname',     443, 'api.duget.org/v1/index.json', 'latest', 'frag', 'user@hostname:443'));
+    TestViaObject('https://hostname:443/api.duget.org/v1/index.json?latest#frag',      _UriValues.Init('https', '',     'hostname',     443, 'api.duget.org/v1/index.json', 'latest', 'frag', 'hostname:443'));
+    TestViaObject('https://user@hostname/api.duget.org/v1/index.json?latest#frag',     _UriValues.Init('https', 'user', 'hostname',      -1, 'api.duget.org/v1/index.json', 'latest', 'frag', 'user@hostname'));
+  end;
+
+
+  procedure TUriTests.UriValuesAreCorrectlyIdentifiedInStringsUsingInterface;
+  begin
+    TestViaInterface('file:///c:/folder/subfolder', _UriValues.Init('file', '', '', -1, 'c:/folder/subfolder', '', '', ''));
+    TestViaInterface('c:\duget\whatever',           _UriValues.Init('file', '', '', -1, 'c:/duget/whatever',   '', '', ''));
+
+    TestViaInterface('http://api.duget.org/v1/index.json',                                _UriValues.Init('http',  '',         'api.duget.org', -1, 'v1/index.json',               '',       '',     'api.duget.org'));
+    TestViaInterface('http://user@hostname:443/api.duget.org/v1/index.json?latest#frag',  _UriValues.Init('http',  'user',     'hostname',     443, 'api.duget.org/v1/index.json', 'latest', 'frag', 'user@hostname:443'));
+    TestViaInterface('https://hostname:443/api.duget.org/v1/index.json?latest#frag',      _UriValues.Init('https', '',         'hostname',     443, 'api.duget.org/v1/index.json', 'latest', 'frag', 'hostname:443'));
+    TestViaInterface('https://user@hostname/api.duget.org/v1/index.json?latest#frag',     _UriValues.Init('https', 'user',     'hostname',      -1, 'api.duget.org/v1/index.json', 'latest', 'frag', 'user@hostname'));
+  end;
+
+
+  procedure TUriTests.EmptyPasswordIsStrippedFromUserInfo;
+  begin
+    TestViaObject('https://user:@hostname/api.duget.org/v1/index.json?latest#frag',       _UriValues.Init('https', 'user',     'hostname',      -1, 'api.duget.org/v1/index.json', 'latest', 'frag', 'user@hostname'));
+    TestViaInterface('https://user:@hostname/api.duget.org/v1/index.json?latest#frag',    _UriValues.Init('https', 'user',     'hostname',      -1, 'api.duget.org/v1/index.json', 'latest', 'frag', 'user@hostname'));
+  end;
+
+
+  procedure TUriTests.NonEmptyPasswordIsPreservedInUserInfo;
+  begin
+    TestViaObject('https://user:pwd@hostname/api.duget.org/v1/index.json?latest#frag', _UriValues.Init('https', 'user:pwd', 'hostname',      -1, 'api.duget.org/v1/index.json', 'latest', 'frag', 'user:pwd@hostname'));
+    TestViaInterface('https://user:pwd@hostname/api.duget.org/v1/index.json?latest#frag', _UriValues.Init('https', 'user:pwd', 'hostname',      -1, 'api.duget.org/v1/index.json', 'latest', 'frag', 'user:pwd@hostname'));
+  end;
+
+
+  procedure TUriTests.UncPathIsParsedAsFileScheme;
+  begin
+    TestViaObject('\\hostname\share\path', _UriValues.Init('file', '', 'hostname', -1, 'share/path', '', '', 'hostname'));
+    TestViaInterface('\\hostname\share\path', _UriValues.Init('file', '', 'hostname', -1, 'share/path', '', '', 'hostname'));
+  end;
+
+
+  procedure TUriTests.DriveLetterPathIsParsedAsFileScheme;
+  begin
+    TestViaObject('c:\folder\path', _UriValues.Init('file', '', '', -1, 'c:/folder/path', '', '', ''));
+    TestViaInterface('c:\folder\path', _UriValues.Init('file', '', '', -1, 'c:/folder/path', '', '', ''));
+  end;
+
+
+  procedure TUriTests.AsPathRaisesInvalidOperationForInvalidUris;
+  var
+    sut: IUri;
+
+    procedure TestWithPath(const aPath: String);
+    begin
+      try
+        sut.AsString := aPath;
+        sut.AsFilePath;
+
+      except
+        Test.RaisedException(EInvalidOperation);
+      end;
+    end;
+
+  begin
+    sut := TUri.Create;
+
+    TestWithPath('');
+    TestWithPath('\\host');
+    TestWithPath('\\host\');
+    TestWithPath('c:');
+  end;
+
+
+  procedure TUriTests.AsPathReturnsFilepathForValidUris;
+  var
+    sut: IUri;
+
+    procedure TestWithPath(const aTestPath: String; const aHost: String; const aPath: String);
+    begin
+      sut.AsString := aTestPath;
+
+      Test('({filepath}).Scheme', [aTestPath]).Assert(sut.Scheme).Equals('file');
+      Test('({filepath}).Host', [aTestPath]).Assert(sut.Host).Equals(aHost);
+      Test('({filepath}).Path', [aTestPath]).Assert(sut.Path).Equals(aPath);
+      Test('({filepath}).AsFilePath', [aTestPath]).Assert(sut.AsFilePath).Equals(aTestPath);
+    end;
+
+  begin
+    sut := TUri.Create;
+
+    TestWithPath('c:\folder', '', 'c:/folder');
+    TestWithPath('\\hostname\share\path', 'hostname', 'share/path');
+    TestWithPath('\\\share\path', '', 'share/path');
+  end;
+
+
+
+
+end.
